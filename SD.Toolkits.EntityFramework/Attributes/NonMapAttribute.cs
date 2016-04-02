14424c692558bd43377ca171647db4700981ef87
Lee <lishilei0523@163.com>
2016-04-02 16:21:35 +0800

    Init

---
+++ SD.Toolkits.EntityFramework/Attributes/NonMapAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SD.Toolkits.EntityFramework.Attributes
+{
+    /// <summary>
+    /// 不映射表特性
+    /// </summary>
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public class NonMapAttribute : Attribute
+    {
+
+    }
+}
---
+++ SD.Toolkits.EntityFrameworkTests/StubEntities/Base/AggregateRootEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SD.Toolkits.EntityFrameworkTests.StubEntities.Base
+{
+    /// <summary>
+    /// 聚合根实体基类
+    /// </summary>
+    [Serializable]
+    public abstract class AggregateRootEntity : PlainEntity
+    {
+
+    }
+}
