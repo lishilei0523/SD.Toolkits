e8ddba137eab7e964eae1ffbbd56ba5b35cd7675
Lee <lishilei0523@163.com>
2016-04-21 10:19:26 +0800

    添加Session共享测试网站

---
+++ SD.Toolkits.SessionSharing.TestSiteMain/Default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+
+namespace SD.Toolkits.SessionSharing.TestSiteMain
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.Session["SessionSharing"] = "Hello world";
+            this.Response.Write(this.Session["SessionSharing"]);
+        }
+    }
+}
---
+++ SD.Toolkits.SessionSharing.TestSiteSub/Default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.UI;
+
+namespace SD.Toolkits.SessionSharing.TestSiteSub
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.Response.Write(this.Session["SessionSharing"]);
+        }
+    }
+}
