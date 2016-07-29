359a98815fac13a5f09173626b9a4dce6b882cec
Lee <lishilei0523@163.com>
2016-07-30 00:46:01 +0800

    -

--- SD.Toolkits.Redis/Properties/AssemblyInfo.cs
+++ SD.Toolkits.Redis/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的常规信息通过以下
@@ -33,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("SD.Toolkits.RedisTests")]
