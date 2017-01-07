0b17affea7044279f17a5cf345b28e5cec587033
Lee <lishilei0523@163.com>
2017-01-07 23:49:38 +0800

    补充注释

--- SD.Toolkits.SessionSharing.TestSiteSub/Default.aspx.cs
+++ SD.Toolkits.SessionSharing.TestSiteSub/Default.aspx.cs
@@ -7,6 +7,14 @@ namespace SD.Toolkits.SessionSharing.TestSiteSub
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            /***
+             * 测试说明：
+             * 主站以SessionSharing为键将"Hello world"存入Session，
+             * 子站以SessionSharing为键读取Session，
+             * 子站如果可以读到主站存放的Session，
+             * 则说明两站点Session共享成功。
+             * **/
+
             this.Response.Write(this.Session["SessionSharing5"]);
         }
     }
