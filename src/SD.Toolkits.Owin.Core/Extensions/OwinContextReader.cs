a04b8e6329e6b6b4d0207211c415294d2a454e38
Lee <lishilei0523@163.com>
2021-03-26 21:22:38 +0800

    添加ASP.NET Core OWIN扩展工具；

---
+++ src/SD.Toolkits.Owin.Core/Extensions/OwinContextReader.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading;
+
+namespace SD.Toolkits.Owin.Core.Extensions
+{
+    /// <summary>
+    /// OwinContext读取器
+    /// </summary>
+    public static class OwinContextReader
+    {
+        /// <summary>
+        /// 同步锁
+        /// </summary>
+        private static readonly object _Sync = new object();
+
+        /// <summary>
+        /// OwinContext异步线程缓存
+        /// </summary>
+        private static readonly AsyncLocal<HttpContext> _OwinContext = new AsyncLocal<HttpContext>();
+
+        /// <summary>
+        /// 设置OwinContext
+        /// </summary>
+        internal static void SetOwinContext(HttpContext owinContext)
+        {
+            lock (_Sync)
+            {
+                _OwinContext.Value = owinContext;
+            }
+        }
+
+        /// <summary>
+        /// 清除OwinContext
+        /// </summary>
+        internal static void ClearOwinContext()
+        {
+            lock (_Sync)
+            {
+                _OwinContext.Value = null;
+            }
+        }
+
+        /// <summary>
+        /// 获取当前OwinContext
+        /// </summary>
+        public static HttpContext Current
+        {
+            get
+            {
+                lock (_Sync)
+                {
+                    HttpContext owinContext = _OwinContext.Value;
+
+                    return owinContext;
+                }
+            }
+        }
+    }
+}
---
+++ src/SD.Toolkits.Owin.Core/Middlewares/CacheOwinContextMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using SD.Toolkits.Owin.Core.Extensions;
+using System.Threading.Tasks;
+
+namespace SD.Toolkits.Owin.Core.Middlewares
+{
+    /// <summary>
+    /// 缓存OwinContext中间件
+    /// </summary>
+    public class CacheOwinContextMiddleware
+    {
+        /// <summary>
+        /// 请求委托
+        /// </summary>
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// 依赖注入构造器
+        /// </summary>
+        public CacheOwinContextMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        /// <summary>
+        /// 执行中间件
+        /// </summary>
+        public async Task Invoke(HttpContext context)
+        {
+            OwinContextReader.SetOwinContext(context);
+
+            await this._next.Invoke(context);
+
+            OwinContextReader.ClearOwinContext();
+        }
+    }
+}
