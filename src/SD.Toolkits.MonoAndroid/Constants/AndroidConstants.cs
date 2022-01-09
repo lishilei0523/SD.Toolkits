19bc83bfb5a37241878b553aa8ff9b696bd056df
Lee <lishilei0523@163.com>
2022-01-09 20:02:50 +0800

    添加Xamarin.Android基础类库；

---
+++ src/SD.Toolkits.MonoAndroid/Constants/AndroidConstants.cs
@@ -0,0 +1,29 @@
+﻿// ReSharper disable once CheckNamespace
+namespace SD.Toolkits.MonoAndroid
+{
+    /// <summary>
+    /// Android常量
+    /// </summary>
+    public static class AndroidConstants
+    {
+        /// <summary>
+        /// 条码广播字段名称
+        /// </summary>
+        public const string BarcodeActionName = "BarcodeActionName";
+
+        /// <summary>
+        /// 条码数据字段名称
+        /// </summary>
+        public const string BarcodeExtraName = "BarcodeExtraName";
+
+        /// <summary>
+        /// 默认条码广播字段
+        /// </summary>
+        public const string DefaultBarcodeAction = "BarcodeAction";
+
+        /// <summary>
+        /// 默认条码数据字段
+        /// </summary>
+        public const string DefaultBarcodeExtra = "BarcodeExtra";
+    }
+}
---
+++ src/SD.Toolkits.MonoAndroid/Constants/BarcodeSetting.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Essentials;
+
+// ReSharper disable once CheckNamespace
+namespace SD.Toolkits.MonoAndroid
+{
+    /// <summary>
+    /// 条码扫描设置
+    /// </summary>
+    public static class BarcodeSetting
+    {
+        /// <summary>
+        /// 条码广播字段名称
+        /// </summary>
+        public static string BarcodeActionName
+        {
+            get => SecureStorage.GetAsync(AndroidConstants.BarcodeActionName).Result;
+            set => SecureStorage.SetAsync(AndroidConstants.BarcodeActionName, value).Wait();
+        }
+
+        /// <summary>
+        /// 条码数据字段名称
+        /// </summary>
+        public static string BarcodeExtraName
+        {
+            get => SecureStorage.GetAsync(AndroidConstants.BarcodeExtraName).Result;
+            set => SecureStorage.SetAsync(AndroidConstants.BarcodeExtraName, value).Wait();
+        }
+    }
+}
