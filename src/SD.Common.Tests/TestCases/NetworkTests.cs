10c8b4c1dbdfc7f97d184eae3c30796233eda410
Lee <lishilei0523@163.com>
2022-05-28 16:48:22 +0800

    基础类库增加获取MAC地址方法；

---
+++ src/SD.Common.Tests/TestCases/NetworkTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SD.Common.Tests.TestCases
+{
+    /// <summary>
+    /// 网络测试
+    /// </summary>
+    [TestClass]
+    public class NetworkTests
+    {
+        /// <summary>
+        /// 获取MAC测试
+        /// </summary>
+        [TestMethod]
+        public void TestGetMacs()
+        {
+            IList<string> macs = NetworkExtension.GetMacs();
+            Trace.WriteLine(macs);
+        }
+
+        /// <summary>
+        /// 获取IP测试
+        /// </summary>
+        [TestMethod]
+        public void TestGetIps()
+        {
+            IList<string> ips = NetworkExtension.GetIPs();
+            Trace.WriteLine(ips);
+        }
+    }
+}
---
+++ src/SD.Common/NetworkExtension.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace SD.Common
+{
+    /// <summary>
+    /// 网络扩展
+    /// </summary>
+    public static class NetworkExtension
+    {
+        #region # 获取本机MAC地址列表 —— static string GetLocalMacAddress()
+        /// <summary>
+        /// 获取本机MAC地址列表
+        /// </summary>
+        /// <returns>本机MAC地址列表</returns>
+        /// <remarks>以“,”分隔</remarks>
+        public static string GetLocalMacAddress()
+        {
+            IList<string> macs = GetMacs();
+            string macsText = macs.ToSplicString();
+
+            return macsText;
+        }
+        #endregion
+
+        #region # 获取本机MAC地址列表 —— IList<string> GetMacs()
+        /// <summary>
+        /// 获取本机MAC地址列表
+        /// </summary>
+        /// <returns>本机MAC地址列表</returns>
+        /// <remarks>以“,”分隔</remarks>
+        public static IList<string> GetMacs()
+        {
+            ICollection<string> macs = new HashSet<string>();
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface networkInterface in interfaces)
+            {
+                PhysicalAddress physicalAddress = networkInterface.GetPhysicalAddress();
+                byte[] addressBytes = physicalAddress.GetAddressBytes();
+                string mac = BitConverter.ToString(addressBytes);
+                macs.Add(mac);
+            }
+
+            return macs.ToList();
+        }
+        #endregion
+
+        #region # 获取本机IP地址列表 —— static string GetLocalIPAddress()
+        /// <summary>
+        /// 获取本机IP地址列表
+        /// </summary>
+        /// <returns>本机IP地址列表</returns>
+        /// <remarks>以“,”分隔</remarks>
+        public static string GetLocalIPAddress()
+        {
+            ICollection<string> ips = GetIPs();
+            string ipsStr = ips.ToSplicString();
+
+            return ipsStr;
+        }
+        #endregion
+
+        #region # 获取本机IP地址列表 —— static IList<string> GetIPs()
+        /// <summary>
+        /// 获取本机本机IP地址
+        /// </summary>
+        /// <returns>本机IP地址列表</returns>
+        public static IList<string> GetIPs()
+        {
+            ICollection<string> ips = new HashSet<string>();
+
+            string hostName = Dns.GetHostName();//本机名   
+            ips.Add(hostName);
+
+            IPAddress[] ipAddresses = Dns.GetHostAddresses(hostName);//会返回所有地址，包括IPv4和IPv6
+            foreach (IPAddress ipAddress in ipAddresses)
+            {
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ips.Add(ipAddress.ToString());
+                }
+            }
+
+            return ips.ToList();
+        }
+        #endregion
+    }
+}
