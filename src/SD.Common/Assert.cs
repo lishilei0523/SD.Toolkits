0f3baedf2396d73decec3faf4861cfa3622e2b02
Lee <lishilei0523@163.com>
2023-09-13 09:57:10 +0800

    -

--- src/SD.Common/Assert.cs
+++ src/SD.Common/Assert.cs
@@ -179,27 +179,4 @@ namespace SD.Common
         }
         #endregion
     }
-
-
-    /// <summary>
-    /// 断言失败异常
-    /// </summary>
-    [Serializable]
-    public class AssertFailedException : ApplicationException
-    {
-        /// <summary>
-        /// 无参构造器
-        /// </summary>
-        public AssertFailedException() { }
-
-        /// <summary>
-        /// 基础构造器
-        /// </summary>
-        /// <param name="message"></param>
-        public AssertFailedException(string message)
-            : base(message)
-        {
-
-        }
-    }
 }
---
+++ src/SD.Common/AssertFailedException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SD.Common
+{
+    /// <summary>
+    /// 断言失败异常
+    /// </summary>
+    [Serializable]
+    public class AssertFailedException : ApplicationException
+    {
+        /// <summary>
+        /// 无参构造器
+        /// </summary>
+        public AssertFailedException() { }
+
+        /// <summary>
+        /// 基础构造器
+        /// </summary>
+        /// <param name="message">异常消息</param>
+        public AssertFailedException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
