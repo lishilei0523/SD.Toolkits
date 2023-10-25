343398304cfaadf0661bea4f4718eee6340407a3
Lee <lishilei0523@163.com>
2023-10-25 22:49:31 +0800

    Skia占位；

---
+++ src/SD.Toolkits.SkiaDrawing/Enums/ThumbnailMode.cs
@@ -0,0 +1,28 @@
+﻿namespace SD.Toolkits.SkiaDrawing.Enums
+{
+    /// <summary>
+    /// 缩略图模式
+    /// </summary>
+    public enum ThumbnailMode
+    {
+        /// <summary>
+        /// 指定高宽缩放
+        /// </summary>
+        WidthAndHeight = 0,
+
+        /// <summary>
+        /// 指定宽，高按比例
+        /// </summary>
+        Width = 1,
+
+        /// <summary>
+        /// 指定高，宽按比例
+        /// </summary>
+        Height = 2,
+
+        /// <summary>
+        /// 指定高宽裁减
+        /// </summary>
+        Cut = 3
+    }
+}
