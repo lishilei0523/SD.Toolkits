c5134d144c01d472597e50efe66eeae5e572ae1a
Lee <lishilei0523@163.com>
2023-11-27 10:27:05 +0800

    OpenCV扩展；

---
+++ src/SD.Toolkits.OpenCV.Tests/TestCases/ImageTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SD.Toolkits.OpenCV.Tests.TestCases
+{
+    /// <summary>
+    /// 图像测试
+    /// </summary>
+    [TestClass]
+    public class ImageTests
+    {
+
+    }
+}
---
+++ src/SD.Toolkits.OpenCV/SkiaSharpExtension.cs
@@ -0,0 +1,40 @@
+﻿using OpenCvSharp;
+using SkiaSharp;
+using System.IO;
+
+namespace SD.Toolkits.OpenCV
+{
+    /// <summary>
+    /// SkiaSharp扩展
+    /// </summary>
+    public static class SkiaSharpExtension
+    {
+        #region # OpenCV图像映射SKBitmap —— static SKBitmap ToSKBitmap(this Mat matrix)
+        /// <summary>
+        /// OpenCV图像映射SKBitmap
+        /// </summary>
+        public static SKBitmap ToSKBitmap(this Mat matrix)
+        {
+            byte[] imageBytes = matrix.ToBytes();
+            SKBitmap bitmap = SKBitmap.Decode(imageBytes);
+
+            return bitmap;
+        }
+        #endregion
+
+        #region # SKBitmap映射OpenCV图像 —— static Mat ToMatrix(this SKBitmap bitmap...
+        /// <summary>
+        /// SKBitmap映射OpenCV图像
+        /// </summary>
+        public static Mat ToMatrix(this SKBitmap bitmap, SKEncodedImageFormat imageFormat = SKEncodedImageFormat.Jpeg, int quantity = 100)
+        {
+            using MemoryStream stream = new MemoryStream();
+            bitmap.Encode(stream, imageFormat, quantity);
+            byte[] imageBytes = stream.ToArray();
+            Mat matrix = Mat.ImDecode(imageBytes);
+
+            return matrix;
+        }
+        #endregion
+    }
+}
