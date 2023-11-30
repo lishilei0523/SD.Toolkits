923c29a69b75601553707275d49a7fd8d8a0cbd1
Lee <lishilei0523@163.com>
2023-11-30 08:18:34 +0800

    添加相似点比较器；

---
+++ src/SD.Common.Tests/TestCases/PointTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SD.Common.Tests.TestCases
+{
+    /// <summary>
+    /// 坐标点测试
+    /// </summary>
+    [TestClass]
+    public class PointTests
+    {
+        #region # 测试去重相似点 —— void TestDistinctSimilarPoints()
+        /// <summary>
+        /// 测试去重相似点
+        /// </summary>
+        [TestMethod]
+        public void TestDistinctSimilarPoints()
+        {
+            const int threshold = 5;
+            Point a = new Point(10, 20);
+            Point b = new Point(12, 22);
+            Point c = new Point(52, 99);
+            Point d = new Point(300, 500);
+            Point e = new Point(11, 210);
+            Point f = new Point(48, 101);
+            Point g = new Point(50, 100);
+            Point[] points = { a, b, c, d, e, f, g };
+
+            IEqualityComparer<Point> comparer = new SimilarPointsComparer(threshold, threshold);
+            ICollection<Point> distinctedPoints = new HashSet<Point>(points, comparer);
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(distinctedPoints.Count, 4);
+        }
+        #endregion
+    }
+}
---
+++ src/SD.Common/SimilarPointsComparer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SD.Common
+{
+    /// <summary>
+    /// 相似点比较器
+    /// </summary>
+    public class SimilarPointsComparer : IEqualityComparer<Point>
+    {
+        #region # 构造器
+
+        #region 01.创建相似点比较器构造器
+        /// <summary>
+        /// 创建相似点比较器构造器
+        /// </summary>
+        /// <param name="thresholdX">X阈值</param>
+        /// <param name="thresholdY">Y阈值</param>
+        public SimilarPointsComparer(int thresholdX, int thresholdY)
+        {
+            this.ThresholdX = thresholdX;
+            this.ThresholdY = thresholdY;
+        }
+        #endregion
+
+        #endregion
+
+        #region # 属性
+
+        #region X阈值 —— int ThresholdX
+        /// <summary>
+        /// X阈值
+        /// </summary>
+        public int ThresholdX { get; private set; }
+        #endregion
+
+        #region Y阈值 —— int ThresholdY
+        /// <summary>
+        /// Y阈值
+        /// </summary>
+        public int ThresholdY { get; private set; }
+        #endregion
+
+        #endregion
+
+        #region # 方法
+
+        #region 是否相似 —— bool Equals(Point source, Point target)
+        /// <summary>
+        /// 是否相似
+        /// </summary>
+        /// <param name="source">源点</param>
+        /// <param name="target">目标点</param>
+        /// <returns>是否相似</returns>
+        public bool Equals(Point source, Point target)
+        {
+            int offsetX = Math.Abs(target.X - source.X);
+            int offsetY = Math.Abs(target.Y - source.Y);
+            if (offsetX > this.ThresholdX || offsetY > this.ThresholdY)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        #endregion
+
+        #region 获取哈希码 —— int GetHashCode(Point point)
+        /// <summary>
+        /// 获取哈希码
+        /// </summary>
+        public int GetHashCode(Point point)
+        {
+            return 0;
+        }
+        #endregion 
+
+        #endregion
+    }
+}
