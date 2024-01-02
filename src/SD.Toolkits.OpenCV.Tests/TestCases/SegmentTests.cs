d72fc1e852233aae3ebe26cb1c9595189b549198
Lee <lishilei0523@163.com>
2024-01-02 16:04:49 +0800

    颜色分割；

---
+++ src/SD.Toolkits.OpenCV.Tests/TestCases/SegmentTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenCvSharp;
+
+namespace SD.Toolkits.OpenCV.Tests.TestCases
+{
+    /// <summary>
+    /// 分割测试
+    /// </summary>
+    [TestClass]
+    public class SegmentTests
+    {
+        #region # 测试颜色分割 —— void TestColorSegment()
+        /// <summary>
+        /// 测试颜色分割
+        /// </summary>
+        [TestMethod]
+        public void TestColorSegment()
+        {
+            Scalar lowerYellow = new Scalar(20, 43, 46);
+            Scalar upperYellow = new Scalar(34, 255, 255);
+
+            using Mat matrix = Cv2.ImRead("Images/Flag.jpg");
+            using Mat hsvMatrix = matrix.CvtColor(ColorConversionCodes.BGR2HSV);
+            Mat result = hsvMatrix.ColorSegment(lowerYellow, upperYellow);
+
+            Cv2.ImShow("OpenCV颜色分割-原图", matrix);
+            Cv2.ImShow("OpenCV颜色分割-效果图", result);
+            Cv2.WaitKey();
+        }
+        #endregion
+    }
+}
---
+++ src/SD.Toolkits.OpenCV/SegmentExtension.cs
@@ -0,0 +1,31 @@
+﻿using OpenCvSharp;
+
+namespace SD.Toolkits.OpenCV
+{
+    /// <summary>
+    /// 分割扩展
+    /// </summary>
+    public static class SegmentExtension
+    {
+        #region # 颜色分割 —— static Mat ColorSegment(this Mat hsvMatrix, Scalar lowerScalar...
+        /// <summary>
+        /// 颜色分割
+        /// </summary>
+        /// <param name="hsvMatrix">HSV图像矩阵</param>
+        /// <param name="lowerScalar">颜色下限</param>
+        /// <param name="upperScalar">颜色上限</param>
+        /// <returns>分割结果图像矩阵</returns>
+        public static Mat ColorSegment(this Mat hsvMatrix, Scalar lowerScalar, Scalar upperScalar)
+        {
+            using Mat mask = new Mat();
+            Cv2.InRange(hsvMatrix, lowerScalar, upperScalar, mask);
+
+            Mat result = new Mat();
+            Cv2.BitwiseAnd(hsvMatrix, hsvMatrix, result, mask);
+            result = result.CvtColor(ColorConversionCodes.HSV2BGR);
+
+            return result;
+        }
+        #endregion
+    }
+}
