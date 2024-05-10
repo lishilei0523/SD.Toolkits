290bd19ebd4e47122b46a965287d498e0a19d3b6
Lee <lishilei0523@163.com>
2024-05-10 13:59:05 +0800

    添加OpenCV与WPF图像互转测试；

---
+++ src/SD.Toolkits.OpenCV.Tests/TestCases/WPFTests.cs
@@ -0,0 +1,51 @@
+﻿#if NET462_OR_GREATER
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace SD.Toolkits.OpenCV.Tests.TestCases
+{
+    /// <summary>
+    /// WPF测试
+    /// </summary>
+    [TestClass]
+    public class WPFTests
+    {
+        #region # 测试映射BitmapSource —— void TestToBitmapSource()
+        /// <summary>
+        /// 测试映射BitmapSource
+        /// </summary>
+        [TestMethod]
+        public void TestToBitmapSource()
+        {
+            using Mat matrix = Cv2.ImRead("Images/China.jpg");
+            BitmapSource bitmapSource = matrix.ToBitmapSource();
+
+            BitmapEncoder bitmapEncoder = new JpegBitmapEncoder();
+            BitmapFrame bitmapFrame = BitmapFrame.Create(bitmapSource);
+            bitmapEncoder.Frames.Add(bitmapFrame);
+            using FileStream outputStream = File.OpenWrite("Images/China.BS.jpg");
+            bitmapEncoder.Save(outputStream);
+        }
+        #endregion
+
+        #region # 测试映射OpenCV矩阵 —— void TestToMat()
+        /// <summary>
+        /// 测试映射OpenCV矩阵
+        /// </summary>
+        [TestMethod]
+        public void TestToMat()
+        {
+            Uri imageUri = new Uri("Images/China.jpg", UriKind.Relative);
+            BitmapSource bitmapSource = new BitmapImage(imageUri);
+
+            using Mat matrix = bitmapSource.ToMat();
+            matrix.SaveImage("Images/China.BS-MT.jpg");
+        }
+        #endregion
+    }
+}
+#endif
