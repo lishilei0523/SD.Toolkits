6f4dbbf017adbfc44eea1fd2287351b5def849da
Lee <lishilei0523@163.com>
2024-12-11 21:02:58 +0800

    优化；

--- src/SD.Toolkits.AspNetCore/Extensions/HttpRequestExtension.cs
+++ src/SD.Toolkits.AspNetCore/Extensions/HttpRequestExtension.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using System;
 
 namespace SD.Toolkits.AspNetCore.Extensions
@@ -36,6 +38,39 @@ namespace SD.Toolkits.AspNetCore.Extensions
             }
 
             return isAjax;
+        }
+        #endregion
+
+        #region # Controller/Action是否有某特性标签 —— static bool HasAttr<T>(this ActionDescriptor...
+        /// <summary>
+        /// Controller/Action是否有某特性标签
+        /// </summary>
+        /// <typeparam name="T">特性类型</typeparam>
+        /// <param name="actionDescriptor">Action方法元数据</param>
+        /// <returns>是否拥有该特性</returns>
+        public static bool HasAttr<T>(this ActionDescriptor actionDescriptor) where T : Attribute
+        {
+            Type type = typeof(T);
+            if (actionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+            {
+                bool actionDefined = controllerActionDescriptor.MethodInfo.IsDefined(type, true);
+                if (actionDefined)
+                {
+                    return true;
+                }
+
+                bool controllerDefined = controllerActionDescriptor.ControllerTypeInfo.IsDefined(type, true);
+                if (controllerDefined)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return false;
         }
         #endregion
     }
